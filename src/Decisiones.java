91df03ed94a42917705834fd6c8f8796ffa74cb5
Jonathan Martinez <marscompereira@gmail.com>
2025-10-16 10:26:09 -0500

    peliculas

---
+++ src/Decisiones.java
@@ -0,0 +1,14 @@
+public class Decisiones {
+
+    public static void main(String[] args) {
+        int fechaLanzamiento = 1999;
+        boolean incluidoEnPlan = true;
+        double notaDePelicula = 8.2;
+
+        if (fechaLanzamiento >= 2022){
+            System.out.println("Peliculas mas populares");
+        }else {
+            System.out.println("Peliculas retro que aun vale la pena ver");
+        }
+    }
+}
