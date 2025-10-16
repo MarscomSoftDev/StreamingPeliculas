e73d91a36453f7b2296d1312c4fd5dd0e99c477f
Jonathan Martinez <marscompereira@gmail.com>
2025-10-16 08:01:42 -0500

    Corrigiendo Main.java con los cálculos de películas

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,14 @@ public class Main{
     public static void main(String[] args){
         System.out.println("Bienvenido (a) a Screen Match");
         System.out.println("Pelicula: Matrix");
+
+        int fechaLanzamiento = 1999;
+        boolean incluidoEnPlan = true;
+        double notaDePelicula = 8.2;
+
+        double mediaDePelicuas = (8.2 + 6.0 + 9.3)/3;
+
+        System.out.println("la media de las peliculas es:" + mediaDePelicuas);
     }
+
 }
