5c704b7070e070942b026fa499b4546e49ff1e5d
Jonathan Martinez <marscompereira@gmail.com>
2025-10-17 10:29:26 -0500

    scanner x = new Scanner

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -4,11 +4,18 @@ public class Decisiones {
         int fechaLanzamiento = 1999;
         boolean incluidoEnPlan = true;
         double notaDePelicula = 8.2;
+        String tipoPlan = "plus";
 
-        if (fechaLanzamiento >= 2022){
-            System.out.println("Peliculas mas populares");
+//        if (fechaLanzamiento >= 2022){
+//            System.out.println("Peliculas mas populares");
+//        }else {
+//            System.out.println("Peliculas retro que aun vale la pena ver");
+//        }
+        if(incluidoEnPlan && tipoPlan.equals("plus")){
+            System.out.println("Distruta de tu pelicula");
+
         }else {
-            System.out.println("Peliculas retro que aun vale la pena ver");
+            System.out.println("pelicula no disponible en tu plan actual");
         }
     }
 }
