2ab4bcbaa7404963caff4b3654ca6a3906550225
Jonathan Martinez <marscompereira@gmail.com>
2025-11-22 14:10:45 -0500

    ciclo for

--- src/Decisiones.java
+++ src/Decisiones.java
@@ -5,11 +5,18 @@ public class Decisiones {
         int fechaDeLanzamiento = 1999;
         boolean incluidoEnPlan = true;
         double notaDeLaPelicula = 3.9;
+        String tipoPlan = "plus";
 
-        if (fechaDeLanzamiento > 2022){
+        if (fechaDeLanzamiento >= 2022){
             System.out.println("Peliculas mas populares");
         }else {
             System.out.println("Peliculas retro que aun vale la pena ver");
+        }
+
+        if (incluidoEnPlan && tipoPlan.equals("plus")){
+            System.out.println("disfruta de tu pelicula");
+        } else {
+            System.out.println("No disponible en tu plan actual");
         }
 
     }
