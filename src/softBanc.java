ba50558ae598821ba425b7f7e454c0701c4deab3
Jonathan Martinez <marscompereira@gmail.com>
2025-11-24 00:25:02 -0500

    declarando datos del cliente

---
+++ src/softBanc.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class softBanc {
+    public  static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+
+      String nombre = "Tony Stark";
+      String tipoDeCuenta = "Corriente";
+      double saldo = 1599.99;
+
+        System.out.println("**********************");
+        System.out.println("\nNombre del cliente: " + nombre);
+        System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
+        System.out.println("Saldo: " + saldo);
+
+    }
+}
