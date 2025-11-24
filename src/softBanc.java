b9dd65364a7983604928c532c3b038de0a14012c
Jonathan Martinez <marscompereira@gmail.com>
2025-11-24 08:44:58 -0500

    menu cliente

--- src/softBanc.java
+++ src/softBanc.java
@@ -2,16 +2,31 @@ import java.util.Scanner;
 
 public class softBanc {
     public  static void main(String[] args) {
-        Scanner teclado = new Scanner(System.in);
 
       String nombre = "Tony Stark";
       String tipoDeCuenta = "Corriente";
       double saldo = 1599.99;
+      int opcion = 0;
 
         System.out.println("**********************");
         System.out.println("\nNombre del cliente: " + nombre);
         System.out.println("El tipo de cuenta es: " + tipoDeCuenta);
-        System.out.println("Saldo: " + saldo);
+        System.out.println("Saldo: " + saldo + "$");
+
+        System.out.println("\n**********************");
+
+        String menu = """
+                *** Escriba el numero de la opcion deseada ***
+                1 - Consultar Saldo
+                2 - Retirar
+                3 - Depositar
+                9 - Salir
+                """;
+        Scanner teclado = new Scanner(System.in);
+        while (opcion != 9){
+            System.out.println(menu);
+            opcion = teclado.nextInt();
+        }
 
     }
 }
